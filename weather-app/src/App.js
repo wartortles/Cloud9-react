747e5e8e3173f2d579f560d31249c2fdd393ee03
Daniel Risdon <drisdon07@gmail.com>
2017-10-22 18:54:26 -0400

    add deleting

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,148 +1,120 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import Saved from './Components/Saved';
-import SearchBar from './Components/SearchBar';
-import SearchResults from './Components/SearchResults';
-import DetailedView from './Components/DetailedView';
+import Saved from './components/Saved';
+import SearchBar from './components/SearchBar';
+import SearchResults from './components/SearchResults';
+import DetailedView from './components/DetailedView';
 
 import axios from 'axios';
 import fab from './images/fab.png';
 
 class App extends Component {
-	constructor(props) {
-		super(props)
+  constructor(props) {
+    super(props)
 
-		this.state = {
-			mode: "viewAll",
-			results: []
-		}
+    this.state = {
+      mode: "viewAll",
+      results: []
+    }
 
-		this.linkToAll = this.linkToAll.bind(this);
-		this.linkToPage = this.linkToPage.bind(this);
-		this.addNew = this.addNew.bind(this);
-		this.goToSearch = this.goToSearch.bind(this);
+    this.linkToAll = this.linkToAll.bind(this);
+    this.linkToPage = this.linkToPage.bind(this);
+    this.addNew = this.addNew.bind(this);
+    this.goToSearch = this.goToSearch.bind(this);
     this.getResults = this.getResults.bind(this);
     this.saveLocation = this.saveLocation.bind(this);
     this.searchWithInput = this.searchWithInput.bind(this);
-    // this.delete = this.delete.bind(this);
-    this.onDelete = this.onDelete.bind(this);
-	};
+    this.delete = this.delete.bind(this);
+  };
 
+  linkToAll() {
+    this.setState({mode: "viewAll"});
+  };
 
-linkToAll() {
-	this.setState({
-		mode: "viewAll"
-	});
-};
+  linkToPage(weatherId) {
+    this.setState({mode: "weatherPage", weatherId: weatherId});
+  };
 
-linkToPage(weatherId) {
-	this.setState({
-		mode: "weatherPage",
-		weatherId: weatherId
-	});
-};
+  addNew(event) {
+    event.preventDefault();
+    this.setState({mode: "createNew"})
+  }
 
-addNew(event) {
-	event.preventDefault();
-	this.setState({
-		mode: "createNew"
-	})
-}
-
-//  delete(results){
-//     console.log(`DELETED:, ${results}`);
-//     axios.delete(`http://localhost:8080/weather/${results.id}`)
-//       .then(res => {
-//         this.setState(state => {
-//           state.results = state.results.filter(s => s.id !== results.id);
-//           state.mode = 'viewAll';
-          
-//           return prev;
-//         });
-//      })
-// }
-
- onDelete(id) {
-        axios.delete('http://localhost:8080/weather')
-            .then((data) => {
-                let weatherData = this.state.weatherData.filter((post) => {
-                    return id !== post.id;
-                });
-
-                this.setState(state => {
-                    state.weatherData = weatherData;
-                    return state;
-                });
-            })
-            .catch((err) => {
-                console.error('err', err);
-            });
-    }
-
-goToSearch() {
-	this.setState({
-		mode:"searchAll"
-	})
-}
-
+  goToSearch() {
+    this.setState({mode: "searchAll"})
+  }
 
   // populate autofilled search results
   getResults(input) {
     if (input.length === 0) {
-      this.setState({ results: [] }, () => console.log(this.state.results));
-    }
-    else { axios.get(`http://localhost:8080/search/${input}`).then(response => {
-      this.setState({ results: response.data.results }, () => console.log(this.state.results));
+      this.setState({
+        results: []
+      }, () => console.log(this.state.results));
+    } else {
+      axios.get(`http://localhost:8080/search/${input}`).then(response => {
+        this.setState({
+          results: response.data.results
+        }, () => console.log(this.state.results));
       })
     }
   }
   // save a location by clicking on it
   saveLocation(placeId, name) {
-    axios.post(`http://localhost:8080/search/geocode/${placeId}`, { name: name }).then(response => {
-      this.setState({ mode: "viewAll", results: [] });
-      })
+    axios.post(`http://localhost:8080/search/geocode/${placeId}`, {name: name}).then(response => {
+      this.setState({mode: "viewAll", results: []});
+    })
   }
   // search for an save a location by text entry
   searchWithInput(input) {
     axios.get(`http://localhost:8080/search/single/${input}`).then(response => {
-      this.setState({ mode: "viewAll", results: '' }, () => console.log(this.state.results));
-      })
+      this.setState({
+        mode: "viewAll",
+        results: ''
+      }, () => console.log(this.state.results));
+    })
+  }
+
+  delete(id) {
+    axios.delete(`http://localhost:8080/weather/${id}`).then(
+      response => {
+        this.setState({ mode: 'viewAll' });
+      }
+    )
   }
 
   render() {
 
-let content;
+    let content;
 
-		const mode = this.state.mode;
-		if (mode === "viewAll") {
-			content = <Saved linkToPage={this.linkToPage} addNew={this.addNew} goToSearch={this.goToSearch} />;
-		} else if (mode === "searchAll") {
-			content = 
+    const mode = this.state.mode;
+    if (mode === "viewAll") {
+      content = <Saved delete={this.delete} linkToPage={this.linkToPage} addNew={this.addNew} goToSearch={this.goToSearch}/>;
+    } else if (mode === "searchAll") {
+      content =
       <div>
-      <div className='dimmed' onClick={this.linkToAll}>
-      <Saved className='dimmed' linkToPage={this.linkToPage} addNew={this.addNew} goToSearch={this.goToSearch} />
+        <div className='dimmed' onClick={this.linkToAll}>
+          <Saved delete={this.delete} className='dimmed' linkToPage={this.linkToPage} addNew={this.addNew} goToSearch={this.goToSearch}/>
+        </div>
+        <div className='search-div'>
+          <SearchBar linkToAll={this.linkToAll} getResults={this.getResults} searchWithInput={this.searchWithInput}/>
+          <SearchResults saveLocation={this.saveLocation} results={this.state.results}/>
+        </div>
       </div>
-      <div className='search-div'>
-        <SearchBar linkToAll={this.linkToAll} getResults={this.getResults} searchWithInput={this.searchWithInput} />
-        <SearchResults saveLocation={this.saveLocation} results={this.state.results}/>
-        </div>
-        </div>
-		} else if (mode === "weatherPage") {
-			content = <DetailedView id={this.state.weatherId} linkToAll={this.linkToAll} />
-		}
+    }
+    // else if (mode === "weatherPage") {
+    //   content = <ShowPage id={this.state.weatherId} linkToAll={this.linkToAll}/>
+    // }
 
     return (
 
       <div className="App">
 
-
-      {content}
+        {content}
 
         <img src={fab} className="add_logo" alt="add_logo" onClick={this.goToSearch}/>
       </div>
     );
   }
 }
-
 
 export default App;
--- weather-app/src/components/Saved.js
+++ weather-app/src/components/Saved.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import bar from './images/bar.png';
 import fab from './images/fab.png';
 import del from './images/delete.png';
@@ -13,188 +13,195 @@ import sleet from './images/sleet.png';
 import snow from './images/snow.png';
 import clearday from './images/clear-day.png';
 import wind from './images/wind.png';
+import wartortle from './images/wartortle.png'
 import axios from 'axios';
 
 class Saved extends Component {
-    constructor(props){
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            locations: []
+    this.state = {
+      locations: []
+    }
+
+    this.onClick = this.onClick.bind(this);
+		this.delete = this.delete.bind(this);
+
+  }
+
+  componentDidMount() {
+    //console.log("THIS DID MOUNT!");
+    axios.get('http://localhost:8080/weather').then(response => {
+      console.log(`Mount: `, response.data)
+
+      const result = response.data.weatherData.map((elem, index) => {
+        let icon = elem.currently.icon;
+        let style;
+        let barStyle;
+
+        if (icon === "clear-day") {
+          icon = clearday;
+          style = {
+            backgroundColor: '#F67F23'
+          }
+          barStyle = {
+            backgroundColor: '#DE751B'
+          }
+        } else if (icon === "clear-night") {
+          icon = clearnight;
+          style = {
+            backgroundColor: '#607D8B'
+          }
+          barStyle = {
+            backgroundColor: '#567280 '
+          }
+        } else if (icon === "rain") {
+          icon = rain;
+          style = {
+            backgroundColor: '#3DB9F5'
+          }
+          barStyle = {
+            backgroundColor: '#36A8E4'
+          }
+        } else if (icon === "snow") {
+          icon = snow,
+          style = {
+            backgroundColor: '#b048c1'
+          }
+          barStyle = {
+            backgroundColor: '#9c3faa'
+          }
+        } else if (icon === "sleet") {
+          icon = sleet,
+          style = {
+            backgroundColor: '#D0D7DE'
+          }
+          barStyle = {
+            backgroundColor: '#B8C8C8'
+          }
+        } else if (icon === "wind") {
+          icon = wind,
+          style = {
+            backgroundColor: '#87DBBA'
+          }
+          barStyle = {
+            backgroundColor: '#76CAA8'
+          }
+        } else if (icon === "fog") {
+          icon = fog,
+          style = {
+            backgroundColor: '#90A4AD'
+          }
+          barStyle = {
+            backgroundColor: '#82949E'
+          }
+        } else if (icon === "cloudy") {
+          icon = cloudy,
+          style = {
+            backgroundColor: '#90A4AD'
+          }
+          barStyle = {
+            backgroundColor: '#82949E'
+          }
+        } else if (icon === "partly-cloudy-day") {
+          icon = partlycloudyday,
+          style = {
+            backgroundColor: '#ECD23F'
+          }
+          barStyle = {
+            backgroundColor: '#D4C03A'
+          }
+        } else if (icon === "partly-cloudy-night") {
+          icon = partlycloudynight,
+          style = {
+            backgroundColor: '#80CBC4'
+          }
+          barStyle = {
+            backgroundColor: '#6FBBB1'
+          }
         }
 
-        this.onClick = this.onClick.bind(this);
+        const temp = Math.round(elem.currently.temperature);
+        const high = Math.round(elem.daily.data[0].temperatureHigh);
+        const low = Math.round(elem.daily.data[0].temperatureLow);
+        const name = response.data.locations[index].name
+        return (
+          <div key={response.data.weatherData.indexOf(elem)}>
+            <li className="flex-item" style={style} data-id={response.data.locations[index].id} onClick={this.onClick}>
+              <div className="tileInfo">
+                <input className='delete' type="image" src={del} value="Delete" onClick={this.delete} data-id={response.data.locations[index].id}/>
+                <div className="titleViewAll" data-id={response.data.locations[index].id}>{name}</div>
+                <div className="iconViewAll">
+                  <img src={icon} className="viewAll_logo"/>
+                </div>
+                <div className="tempViewAll" data-id={response.data.locations[index].id}>{temp}°</div>
 
-    }
+                <div className="bar_tile" style={barStyle}>
+                  <p data-id={response.data.locations[index].id}>
+                    L {low}° &nbsp;&nbsp; H {high}°</p>
 
+                </div>
 
-componentDidMount() {
-    //console.log("THIS DID MOUNT!");
-    axios.get('http://localhost:8080/weather')
-    .then(response => {
-        console.log(`Mount: `, response.data)
+              </div>
+            </li>
+          </div>
+        )
+      });
 
-        const result = response.data.weatherData.map((elem, index) => {
-            let icon = elem.currently.icon;
-            let style;
-            let barStyle;
+      //console.log(result);
 
-            if (icon === "clear-day"){
-                icon = clearday;
-                style = {
-                    backgroundColor: '#F67F23' ,
-                }
-                barStyle = {
-                    backgroundColor:  '#DE751B' ,
-                }
-            }
-            else if(icon === "clear-night"){
-                icon = clearnight;
-                style = {
-                    backgroundColor: '#607D8B'
-                }
-                barStyle = {
-                    backgroundColor: '#567280 '
-                }
-            }
-            else if(icon === "rain"){
-                icon = rain;
-                style = {
-                    backgroundColor: '#3DB9F5' ,
-                }
-                barStyle = {
-                    backgroundColor: '#36A8E4'
-                }
-            }
-            else if(icon === "snow"){
-                icon= snow,
-                style ={
-                    backgroundColor: '#b048c1',
-                }
-                barStyle ={
-                    backgroundColor:  '#9c3faa' ,
-                }
-            }
-            else if(icon === "sleet"){
-                icon= sleet,
-                style= {
-                    backgroundColor: '#D0D7DE' ,
-                }
-                barStyle={
-                backgroundColor: '#B8C8C8',
-                }
-            }
-            else if(icon === "wind"){
-                icon= wind,
-                style= {
-                    backgroundColor: '#87DBBA',
-                }
-                barStyle= {
-                backgroundColor: '#76CAA8',
-                }
-            }
-            else if(icon === "fog"){
-                icon= fog,
-                style= {
-                backgroundColor: '#90A4AD'
-                }
-                barStyle = {
-                backgroundColor: '#82949E'
-            }
-            }
-            else if(icon === "cloudy"){
-                icon= cloudy,
-                style={
-                    backgroundColor: '#90A4AD'
-                }
-                barStyle={
-                    backgroundColor: '#82949E'
-                }
-            }
-            else if(icon === "partly-cloudy-day"){
-                icon= partlycloudyday,
-                style={
-                    backgroundColor:  '#ECD23F'
-                }
-                barStyle={
-                backgroundColor: '#D4C03A'
-                }
-            }
-            else if(icon === "partly-cloudy-night"){
-                icon= partlycloudynight,
-                style={
-                    backgroundColor: '#80CBC4'
-                }
-                barStyle={
-                backgroundColor: '#6FBBB1'
-                }
-            }
-
-            const temp = Math.round(elem.currently.temperature);
-            const high = Math.round(elem.daily.data[0].temperatureHigh);
-            const low = Math.round(elem.daily.data[0].temperatureLow);
-            const name = response.data.locations[index].name
-            return (
-                    <div>
-                    <li className="flex-item" style={style} data-id={response.data.locations[index].id} key={response.data.weatherData.indexOf(elem)} onClick={this.onClick}>
-                       <div className="tileInfo">
-                            <input class='delete' type="image" src={del} value="Delete" />
-                            <div className="titleViewAll" data-id={response.data.locations[index].id}>{name}</div>
-                            <div className="iconViewAll">
-                                <img src={icon} className="viewAll_logo"/> </div>
-                            <div className="tempViewAll" data-id={response.data.locations[index].id}>{temp}°</div>
-
-                            <div className="bar_tile" style={barStyle}>
-                                <p data-id={response.data.locations[index].id}> L {low}° &nbsp;&nbsp; H {high}°</p>
-
-                            </div>
-
-                        </div>
-                    </li>
-                    </div>
-            )
-        });
-
-        //console.log(result);
-
-        this.setState({
-            locations: result
-        }, () => {console.log(this.state.locations)})
+      this.setState({
+        locations: result
+      }, () => {
+        console.log(this.state.locations)
+      })
     });
-}
+  }
 
-onClick(event) {
-     event.preventDefault();
+  onClick(event) {
+    event.preventDefault();
     this.props.linkToPage(event.target.dataset.id);
     console.log(event.target.dataset.id);
-    }
+  }
 
-
-render() {
-
-    return(
-      <div>
-      <div className="nav-bar-viewAll">
-              <img src={bar} className="bar_logo" alt="bar_logo" />
-              <div className="editButton"> EDIT </div>
-          </div>
-
-        <ul className="flex-container">
-                {this.state.locations}
-
-                <li className="flex-item" id="extraTile">
-                    <div className="extraTile">
-                        <img onClick={this.props.goToSearch} src={addTile} className="addOne_logo" />
-                            <p> GET SOME WEATHER </p>
-                     </div>
-
-                </li>
-      </ul>
-
-      </div>
-        );
-    }
+	delete(event) {
+		this.props.delete(event.target.dataset.id);
 }
 
+  render() {
+
+    return (
+      <div>
+        <div className="nav-bar-viewAll">
+          <img src={bar} className="bar_logo" alt="bar_logo"/>
+          <div className="editButton">
+            EDIT
+          </div>
+        </div>
+
+        <ul className="flex-container">
+          {this.state.locations.length > 0 && this.state.locations}
+
+          {this.state.locations.length > 0 && <li className="flex-item" id="extraTile">
+            <div className="extraTile">
+               <img onClick={this.props.goToSearch} src={addTile} className="addOne_logo"/>
+              <p>
+                GET SOME WEATHER
+              </p>
+            </div>
+
+          </li> }
+        </ul>
+				{this.state.locations.length === 0 &&
+					<div className='wartortle'>
+					<img src={wartortle}/>
+					<p>Want to know if you'll melt outside? <br/>
+				Start getting some weather.</p>
+			</div>}
+
+      	</div>
+    );
+  }
+}
 
 export default Saved;
